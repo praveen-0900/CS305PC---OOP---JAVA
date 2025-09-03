ee3dde1ea06975389311dbe74d3fc7a3bd2bdb13
praveen-0900 <praveen.ala55@gmail.com>
2025-09-03 11:31:58 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,14 @@
+Class Lab1P1{
+	
+}	public static void main(String...args[]){
+        byte = 127;
+        Byte bb = b; // Auto boxing
+        // int x = 200;
+        // byte b = (byte)x;
+        
+        System.Out.println(byte MAX-VALUE);
+        System.Out.println(bb MIN-VALUE);
+	}
+}
+
+	
